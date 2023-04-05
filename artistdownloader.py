760292b2eff6c3f179094c932a78b34281e697fe
Tsjerk Piter Walinga <t.p.walinga@hotmail.com>
2023-04-05 13:57:43 +0200

    fixed for new version of spotdl

--- artistdownloader.py
+++ artistdownloader.py
@@ -2,6 +2,8 @@ from spotdl import Spotdl
 from spotdl import types
 from spotdl import SpotifyClient
 from spotdl import utils
+from spotdl.types.artist import Artist
+from spotdl.types.album import Album
 import sys
 import time
 import os
@@ -9,7 +11,7 @@ import os
 # download all albums from an artist and neatly organize them in ~/Music/ArtistName/AlbumName
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python artistdownloader.py artist")
+        print("Usage: python artistdownloader.py artist_url")
         return
     # login to spotify
     # spotdl.utils.spotify.SpotifyError: Spotify client not created. Call SpotifyClient.init(client_id, client_secret, user_auth, cache_path, no_cache, open_browser) first.
@@ -17,32 +19,33 @@ def main():
     client_id = utils.config.get_config()['client_id']
     client_secret = utils.config.get_config()['client_secret']
     spotdl = Spotdl(client_id, client_secret)
-    artist_search = types.Artist.search(sys.argv[1])
-    artist_url = artist_search['artists']['items'][0]['external_urls']['spotify']
-    artist = types.Artist.from_url(artist_url)
+    #artist_search = Artist.search(sys.argv[1])
+    #artist_url = artist_search['artists']['items'][0]['external_urls']['spotify']
+    artist = Artist.from_url(sys.argv[1])
     print("Downloading all albums from " + artist.name)
-    albums = types.Artist.get_albums(artist_url)
     albumnames = []
-    for album in albums:
+    for album in artist.albums:
+        current = Album.from_url(album)
+        name = Album.get_metadata(album)[0]["name"]
         # skip duplicate albums
-        if types.Album.get_metadata(album)['name'] in albumnames:
+        if name in albumnames:
             continue
-        if "deluxe" in types.Album.get_metadata(album)['name'].lower() \
-            or "bonus" in types.Album.get_metadata(album)['name'].lower() \
-            or "live" in types.Album.get_metadata(album)['name'].lower() \
-            or "version" in types.Album.get_metadata(album)['name'].lower() \
-            or "bundle" in types.Album.get_metadata(album)['name'].lower() \
-            or "single" in types.Album.get_metadata(album)['name'].lower() \
-            or "ep" in types.Album.get_metadata(album)['name'].lower() \
-            or "mix" in types.Album.get_metadata(album)['name'].lower() \
-            or "edit" in types.Album.get_metadata(album)['name'].lower():
+        if "deluxe" in name.lower() \
+            or "bonus" in name.lower() \
+            or "live" in name.lower() \
+            or "version" in name.lower() \
+            or "bundle" in name.lower() \
+            or "single" in name.lower() \
+            or "ep" in name.lower() \
+            or "mix" in name.lower() \
+            or "edit" in name.lower():
             continue
-        albumnames.append(types.Album.get_metadata(album)['name'])
-        print("Downloading album: " + types.Album.get_metadata(album)['name'])
+        albumnames.append(name)
+        print("Downloading album: " + name)
         # create directory for album
-        os.system("mkdir -p -v \"/home/kek/Music/" + artist.name + "/" + types.Album.get_metadata(album)['name'] + "\"")
+        os.system("mkdir -p -v \"/home/kek/Music/" + artist.name + "/" + name + "\"")
         # change directory to album directory
-        os.chdir("/home/kek/Music/" + artist.name + "/" + types.Album.get_metadata(album)['name'])
+        os.chdir("/home/kek/Music/" + artist.name + "/" + name)
         # download album via terminal command
         os.system("spotdl " + album)
 
